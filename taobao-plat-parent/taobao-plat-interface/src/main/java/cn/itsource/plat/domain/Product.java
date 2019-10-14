50cc67f4a2ebf6a7a3b8be0c26046456879442f2
houxingwu <1090406642@qq.com>
2019-10-14 12:42:24 +0800

    sfsfsfsdf

---
+++ taobao-plat-parent/taobao-plat-interface/src/main/java/cn/itsource/plat/domain/Product.java
@@ -0,0 +1,28 @@
+package cn.itsource.plat.domain;
+
+/**
+ * @Description 产品
+ * @Author
+ * @Date 2019-10-14 12:39
+ * @Version 1.0
+ */
+public class Product {
+    private String name;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
